dea87ad4b7401eb7dc0b8c6a53d9d22e6239244f
dleo9307 <dleo9307@naver.com>
2020-05-12 11:23:28 +0900

    Swagger 추가

--- src/books/dto/create-book.dto.ts
+++ src/books/dto/create-book.dto.ts
@@ -1,7 +1,15 @@
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateBookDto {
+  @ApiProperty()
   readonly name: string;
+
+  @ApiProperty()
   readonly author: string;
+
+  @ApiProperty()
   readonly publisher: string;
+
+  @ApiProperty()
   readonly count: number;
 }
